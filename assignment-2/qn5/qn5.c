553709727c5234e7df2f8fbdd7dd3db3b50c94dd
Vaibhav Gupta <vaibhavgupta6393@gmail.com>
2026-01-30 22:20:24 +0530

    solved qn5 of assignment 2

---
+++ assignment-2/qn5/qn5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  float e = 2.718281828;
+  printf("%.0f\n", e);
+  printf("%.0f.\n",round(e));
+  printf("%.1f\n",e);
+  printf("%.2f\n",e);
+  printf("%.6f\n",e);
+  printf("%.6f\n",e);
+  printf("%.7f\n",e);
+  return 0;
+}
